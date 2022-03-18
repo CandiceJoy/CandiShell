7391ffa911fa4b88e0f9f0da53d826cba3bb0133
CandiceJoy <candice@candicejoy.com>
2022-03-18 22:46:57 +0000

    test

--- index.js
+++ index.js
@@ -135,10 +135,12 @@ zshRefresh();
 
 function processArgs()
 {
+console.log("A");
 	const args = process.argv.slice(2);
-	
+	console.log("B"+args.length);
 	for( let i = 0; i < args.length; i++ )
 	{
+		console.log("C");
 		const arg = args[i];
 		console(`arg: '${arg}'`);
 
