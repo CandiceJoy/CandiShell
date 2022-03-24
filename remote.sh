6eff004649ad0330e51d4c116aa8afdf9c9dc6ef
CandiceJoy <candice@candicejoy.com>
2022-03-23 23:43:15 -0500

    updated

--- remote.sh
+++ remote.sh
@@ -5,7 +5,7 @@ original_url=$(cat ./.git/config |grep "url")
 for (( i=0; i<${#domains[@]}; i++ )); do
 	domain=${domains[$i]}
 	domain_clean=${domain//\./\\\.}
-	regex="url = https://(git\.|www\.)?$domain_clean/(.*?)/(.*?)(\.git)?"
+	regex="https://(git\.|www\.)?$domain_clean/(.*)/(.*)(\.git)?"
 
 	if [[ $original_url =~ $regex ]]; then
 		subdomain=${BASH_REMATCH[1]}
@@ -15,13 +15,14 @@ for (( i=0; i<${#domains[@]}; i++ )); do
 	fi
 done
 
-if [ -z $ssh_url ]; then
+if [ -z "$ssh_url" ]; then
+	exit
 	url="$1"
 else
 	url="$ssh_url"
 fi
 
-if [[ $original_url != $url ]]; then
-	git remote set-url origin $url
-	#echo "Set: $url"
+if [[ $original_url != "$url" ]]; then
+	eval git remote set-url origin "$url"
+	echo "Set: $url"
 fi
